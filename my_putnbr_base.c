8d22ee0ef85551a4611a107937e4f1a9feb2192e
David Le <davidle93@gmail.com>
2015-11-03 21:57:24 +0100

    update my_putnbr_base.c

--- my_putnbr_base.c
+++ my_putnbr_base.c
@@ -8,6 +8,61 @@
 ** Last update Tue Nov  3 18:44:42 2015 LE David
 */
 
+#include <unistd.h>
+
+void	my_swap(int *a, int *b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void	my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void	my_putstr(char *str)
+{ 
+  while (*str != '\0')
+    {
+      my_putchar(*str);
+      str++;
+    }
+}
+
+
+
+void	my_put_nbr(int n)
+{
+  int	div;
+  int	min;
+  int	max;
+  
+  div = 1;
+  min = n;
+  if (min == -2147483648)
+    {
+      my_putstr("-2147483648");
+    }
+  else
+    {
+      if (n < 0)
+	{
+	  my_putchar('-');
+	  n = n * (-1);
+	}
+      while ((n / div) >= 10)
+	div = div * 10;
+      while (div > 0)
+	{
+	  my_putchar((n / div) % 10 + 48);
+	  div = div / 10;
+	}
+    }
+}
+
+
 int	my_strcmp(char *s1, char *s2)
 {
   int i;
@@ -28,24 +83,56 @@ int	my_strcmp(char *s1, char *s2)
   return (0);
 }
 
-
-
-
-
-
-
-
-
 void	my_putnbr_base(int nbr, char *base)
 {
   //je compare 1 par 1
   if (my_strcmp(base, "01") == 0)
     {
-      
+      char buffer[1000];
+      int j;
+      j = 0;
+      //tant que nbr est > 0
+      while (nbr > 0)
+	{
+	  // tu regardes le reste de la division
+	  if (nbr % 2 == 0)
+	    {
+	      buffer[j] = '0';
+	      j++;
+	    }
+	  // si il y a un reste, je mets un 1
+	  else if (nbr % 2 == 1)
+	    {
+	      buffer[j] = '1';
+	      j++;
+	    }
+	  //je divise par 2 le nbr
+	  nbr /= 2;
+	}
+      //je ferme le buffer
+      buffer[j] = '\0';
+      j--;
+      //je mets un compteur pour parcourir mon nouveau buffer
+      int i;
+      i = 0;
+      //je créer mon nouveau buffer
+      char buffer2[1000];
+      //je parcours a l'envers mon buffer original et je stocke les valeurs dans le nouveau
+      while (j >= 0)
+	{
+	  buffer2[i] = buffer[j];
+	  i++;
+	  j--;
+	}
+      //je ferme mon deuxieme buffer
+      buffer2[i] = '\0';
+      //je l'affiche
+      my_putstr(buffer2);
+
     }
   else if (my_strcmp(base, "0123456789") == 0)
     {
-      
+      my_put_nbr(nbr);
     }
   else if (my_strcmp(base, "0123456789ABCDEF") == 0)
     {
@@ -55,7 +142,7 @@ void	my_putnbr_base(int nbr, char *base)
 
 int	main()
 {
-  my_putnbr_base(35, "01");
+  my_putnbr_base(133, "01");
   my_putchar('\n');
   //my_putnbr_base(35, "0123456789");
   //my_putchar('\n');
